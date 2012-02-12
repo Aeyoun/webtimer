b909798d28affdba7055629dc533ef526971307d
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-12 21:57:54 +0100

    from localStorage to widget.preferences

--- background.js
+++ background.js
@@ -34,42 +34,42 @@
   
   // Add sites with under threshold seconds of total use to "other" category
   function combineEntries(threshold) {
-    var domains = JSON.parse(localStorage["domains"]);
-    var other = JSON.parse(localStorage["other"]);
+    var domains = JSON.parse(widget.preferences.getItem('domains'));
+    var other = JSON.parse(widget.preferences.getItem('other'));
     for (var domain in domains) {
-      var domain_data = JSON.parse(localStorage[domain]);
+      var domain_data = JSON.parse(widget.preferences.getItem(domain));
       if (domain_data.all < threshold) {
         other.all += domain_data.all;
-        delete localStorage[domain];
+        delete widget.preferences(domain);
         delete domains[domain];
       }
     }
-    localStorage["other"] = JSON.stringify(other);
-    localStorage["domains"] = JSON.stringify(domains);
+    widget.preferences.setItem('other', JSON.stringify(other));
+    widget.preferences.setItem('domains', JSON.stringify(domains));
   }
   
   // Check to make sure data is kept for the same day
   function checkDate() {
     var todayStr = new Date().toLocaleDateString();
-    var saved_day = localStorage["date"];
+    var saved_day = widget.preferences.getItem('date');
     if (saved_day !== todayStr) {
       // Reset today's data
-      var domains = JSON.parse(localStorage["domains"]);
+      var domains = JSON.parse(widget.preferences.getItem('domains'));
       for (var domain in domains) {
-        var domain_data = JSON.parse(localStorage[domain]);
+        var domain_data = JSON.parse(widget.preferences.getItem(domain));
         domain_data.today = 0;
-        localStorage[domain] = JSON.stringify(domain_data);
+        widget.preferences.setItem(domain, JSON.stringify(domain_data));
       }
       // Reset total for today
-      var total = JSON.parse(localStorage["total"]);
+      var total = JSON.parse(widget.preferences.getItem('total'));
       total.today = 0;
-      localStorage["total"] = JSON.stringify(total);
+      widget.preferences.setItem('total', JSON.stringify(total));
       // Combine entries with total time less than a minute
       combineEntries(60);
       // Keep track of number of days web timer has been used
-      localStorage["num_days"] = parseInt(localStorage["num_days"]) + 1;
+      widget.preferences.setItem('num_days', parseInt(widget.preferences.getItem('num_days')) + 1);
       // Update date
-      localStorage["date"] = todayStr;
+      widget.preferences.setItem('date', todayStr);
     }
   }
   
@@ -84,7 +84,7 @@
     if (!url.match(/^http/)) {
       return true;
     }
-    var blacklist = JSON.parse(localStorage["blacklist"]);
+    var blacklist = JSON.parse(widget.preferences.getItem('blacklist'));
     for (var i = 0; i < blacklist.length; i++) {
       if (url.match(blacklist[i])) {
         return true;
@@ -107,15 +107,15 @@
               if (!inBlacklist(tab.url)) {
                 var domain = extractDomain(tab.url);
                 // Add domain to domain list if not already present
-                var domains = JSON.parse(localStorage["domains"]);
+                var domains = JSON.parse(widget.preferences.getItem('domains'));
                 if (!(domain in domains)) {
                   // FIXME: Using object as hash set feels hacky
                   domains[domain] = 1;
-                  localStorage["domains"] = JSON.stringify(domains);
+                  widget.preferences.setItem('domains', JSON.stringify(domains));
                 }
                 var domain_data;
-                if (localStorage[domain]) {
-                  domain_data = JSON.parse(localStorage[domain]);
+                if (widget.preferences.getItem(domain)) {
+                  domain_data = JSON.parse(widget.preferences.getItem(domain));
                 } else {
                   domain_data = {
                     today: 0,
@@ -124,12 +124,12 @@
                 }
                 domain_data.today += UPDATE_INTERVAL;
                 domain_data.all += UPDATE_INTERVAL;
-                localStorage[domain] = JSON.stringify(domain_data);
+                widget.preferences.setItem(domain, JSON.stringify(domain_data));
                 // Update total time
-                var total = JSON.parse(localStorage["total"]);
+                var total = JSON.parse(widget.preferences.getItem('total'));
                 total.today += UPDATE_INTERVAL;
                 total.all += UPDATE_INTERVAL;
-                localStorage["total"] = JSON.stringify(total);
+                widget.preferences.setItem('total', JSON.stringify(total));
               }
             });
           }
