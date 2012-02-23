fddb6a76d1ca546882b336363c01db89b263de76
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-23 10:04:15 +0100

    Idle detection from the background service. Limited to window and tab events, plus URL changes.

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,7 @@
 (function() {
-  var toolbarButton,
+  var userSnoozing = false,
+  userSnoozeTimer,
+  currentTabURL,
   toolbarButtonProperties = {
     disabled: false,
     icon: 'icon_19.png',
@@ -12,27 +14,59 @@
   },
   toolbarButton = opera.contexts.toolbar.createItem(toolbarButtonProperties);
   opera.contexts.toolbar.addItem(toolbarButton);
-    
-  
+
+  (function registerEventListeners()
+  {
+    var tabEvents = ['close', 'create', 'focus', 'blur'],
+    windowEvents  = ['create', 'close', 'focus'];
+    for (var event in tabEvents)
+    {
+      opera.extension.tabs.addEventListener(tabEvents[event], restartUserIdleMonitor, false);
+    }
+    for (var event in windowEvents)
+    {
+      opera.extension.windows.addEventListener(windowEvents[event], restartUserIdleMonitor, false);
+    }
+  }());
+
+  function checkCurrentTabURLChange()
+  {
+    if (currentTabURL != opera.extension.tabs.getFocused().url)
+    {
+      currentTabURL = opera.extension.tabs.getFocused().url;
+      restartUserIdleMonitor();
+    }
+  }
+
+  function restartUserIdleMonitor()
+  {
+    userSnoozing = false;
+    clearTimeout(userSnoozeTimer);
+    userSnoozeTimer = setTimeout(function () { userSnoozing = true; }, 60000); // one minute idle timer
+  }
+
   // Interval (in seconds) to update timer
   var UPDATE_INTERVAL = 3;
   
-  setDefaults();
-  function setDefaults() {
+  (function setDate()
+  {
     // Set date
     if (!widget.preferences.getItem('date'))
     {
       widget.preferences.setItem('date', new Date().toLocaleDateString());
     }
-  }
+  }());
   
   // Add sites with under threshold seconds of total use to "other" category
-  function combineEntries(threshold) {
+  function combineEntries(threshold)
+  {
     var domains = JSON.parse(widget.preferences.getItem('domains'));
     var other = JSON.parse(widget.preferences.getItem('other'));
-    for (var domain in domains) {
+    for (var domain in domains)
+    {
       var domain_data = JSON.parse(widget.preferences.getItem(domain));
-      if (domain_data.all < threshold) {
+      if (domain_data.all < threshold)
+      {
         other.all += domain_data.all;
         widget.preferences.setItem(domain, '');
         delete domains[domain];
@@ -43,13 +77,16 @@
   }
   
   // Check to make sure data is kept for the same day
-  function checkDate() {
+  function checkDate()
+  {
     var todayStr = new Date().toLocaleDateString();
     var saved_day = widget.preferences.getItem('date');
-    if (saved_day !== todayStr) {
+    if (saved_day !== todayStr)
+    {
       // Reset today's data
       var domains = JSON.parse(widget.preferences.getItem('domains'));
-      for (var domain in domains) {
+      for (var domain in domains)
+      {
         var domain_data = JSON.parse(widget.preferences.getItem(domain));
         domain_data.today = 0;
         widget.preferences.setItem(domain, JSON.stringify(domain_data));
@@ -69,45 +106,58 @@
   
   // Extract the domain from the url
   // e.g. http://google.com/ -> google.com
-  function extractDomain(url) {
+  function extractDomain(url)
+  {
     var re = /:\/\/(www\.)?(.+?)\//;
     return url.match(re)[2];
   }
   
-  function inBlacklist(url) {
-    if (!url.match(/^http/)) {
+  function inBlacklist(url)
+  {
+    if (!url.match(/^http/))
+    {
       return true;
     }
     var blacklist = JSON.parse(widget.preferences.getItem('blacklist'));
-    for (var i = 0; i < blacklist.length; i++) {
-      if (url.match(blacklist[i])) {
+    for (var i = 0; i < blacklist.length; i++)
+    {
+      if (url.match(blacklist[i]))
+      {
         return true;
       }
     }
     return false;
   }
-  
+
   // Update the data
-  function updateData() {
-    // Only count time if Chrome has focus
-    if (window.focus) {
-      if (opera.extension.tabs.getFocused() && opera.extension.tabs.getFocused().url) {
+  function recordData()
+  {
+    if (opera.extension.tabs.getFocused() && opera.extension.tabs.getFocused().url)
+    {
+      checkCurrentTabURLChange();
+      if (!userSnoozing)
+      {
         var tab = opera.extension.tabs.getFocused();
         // Make sure 'today' is up-to-date
         checkDate();
-        if (!inBlacklist(tab.url)) {
+        if (!inBlacklist(tab.url))
+        {
           var domain = extractDomain(tab.url);
           // Add domain to domain list if not already present
           var domains = JSON.parse(widget.preferences.getItem('domains'));
-          if (!(domain in domains)) {
+          if (!(domain in domains))
+          {
             // FIXME: Using object as hash set feels hacky
             domains[domain] = 1;
             widget.preferences.setItem('domains', JSON.stringify(domains));
           }
           var domain_data;
-          if (widget.preferences.getItem(domain)) {
+          if (widget.preferences.getItem(domain))
+          {
             domain_data = JSON.parse(widget.preferences.getItem(domain));
-          } else {
+          }
+          else
+          {
             domain_data = {
               today: 0,
               all: 0
@@ -126,10 +176,11 @@
     }
   }
   // Update timer data every UPDATE_INTERVAL seconds
-  setInterval(updateData, UPDATE_INTERVAL * 1000);
+  setInterval(recordData, UPDATE_INTERVAL * 1000);
 }());
 
-function openOptionsTab() {
+function openOptionsTab()
+{
   opera.extension.tabs.create( {
     url: 'options.html', focused: true
   } );
