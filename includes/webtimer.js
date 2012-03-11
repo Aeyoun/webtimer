425ed8909e01ef7bc174ade707b816b59f0ae868
Daniel Aleksandersen <code@daniel.priv.no>
2012-03-11 03:25:28 +0100

    JSLint review changes.

--- includes/webtimer.js
+++ includes/webtimer.js
@@ -1,12 +1,10 @@
-(function() {
-
+(function () {
+  'use strict';
   // This script also grants API read access to tabs it is loaded in.
-
   var userDidScroll = false;
-  document.addEventListener('scroll', function() { userDidScroll = true; }, false);
-  setInterval(function() {
-    if (userDidScroll)
-    {
+  document.addEventListener('scroll', function () { userDidScroll = true; }, false);
+  setInterval(function () {
+    if (userDidScroll) {
       userDidScroll = false;
       opera.extension.postMessage('userScrolled');
     }
